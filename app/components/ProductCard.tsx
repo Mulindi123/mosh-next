79a8cd34b0296071f73bf1b6db855ab0f100fa48
Medrine Mulindi <medrine.mulindi@gmail.com>
2024-07-10 21:09:25 +0300

    styling

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,10 +1,11 @@
-
 import React from 'react'
 import AddtoCart from './AddtoCart'
+// import styles from './ProductCard.module.css'
 
 const ProductCard = () => {
+
   return (
-    <div>
+    <div className= "p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-500 rounded-lg">
         <AddtoCart />
     </div>
   )
