e3fd76ff97b4147b0efbfd632b5650e82181d6ab
Medrine Mulindi <medrine.mulindi@gmail.com>
2024-07-10 11:39:28 +0300

    fetches users data

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
+interface User {
+  id: number;
+  name: string;
+  username: string;
 
-const UsersPage = () => {
+}
+
+const UsersPage = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  const users: User[] = await res.json();
+
   return (
-    <div>UsersPage</div>
+    <>
+      <h1>Users</h1>
+      <ul>
+        {users.map(user =>
+          <li key={user.id}>{user.name}</li>
+        )}
+      </ul>
+    </>
   )
 }
 
