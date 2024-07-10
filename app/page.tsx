436cce2cc41d7e8483299400c74440f8d19befe6
Medrine Mulindi <medrine.mulindi@gmail.com>
2024-07-10 11:22:32 +0300

    adds productcard

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+'use Client';
 import Link from "next/link";
+import ProductCard from "./components/ProductCard";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello World</h1>
       <Link href="/users">users</Link>
+      <ProductCard />
     </main>
   )
 }
